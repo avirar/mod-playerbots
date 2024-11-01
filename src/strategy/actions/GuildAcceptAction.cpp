926e8c9fb33a7b5b7f7c9b1c8e17bb5d9fa3bf06
avirar <luke.thorpe@gmail.com>
2024-11-01 15:51:15 +1100

    Update GuildAcceptAction.cpp

--- src/strategy/actions/GuildAcceptAction.cpp
+++ src/strategy/actions/GuildAcceptAction.cpp
@@ -13,47 +13,58 @@
 bool GuildAcceptAction::Execute(Event event)
 {
     WorldPacket p(event.getPacket());
-    p.rpos(0);
+    p.rpos(0);  // Reset read position to the beginning of the packet
+
     Player* inviter = nullptr;
-    std::string Invitedname;
-    p >> Invitedname;
+    std::string invitedName;
+    p >> invitedName;
 
-    if (normalizePlayerName(Invitedname))
-        inviter = ObjectAccessor::FindPlayerByName(Invitedname.c_str());
+    // Log the inviter name parsed from the packet
+    LOG_INFO("playerbots", "Guild invitation packet received. Inviter name: '{}'", invitedName);
 
+    // Normalize and retrieve the player object for the inviter
+    if (normalizePlayerName(invitedName))
+        inviter = ObjectAccessor::FindPlayerByName(invitedName.c_str());
+
+    // Log if inviter is found or not
     if (!inviter)
+    {
+        LOG_ERROR("playerbots", "Inviter '{}' not found. Unable to process guild invitation.", invitedName);
         return false;
+    }
 
+    // Initial guild and bot state checks
     bool accept = true;
     uint32 guildId = inviter->GetGuildId();
     if (!guildId)
     {
+        LOG_ERROR("playerbots", "Inviter '{}' is not in a guild. Cannot accept invitation.", inviter->GetName().c_str());
         botAI->TellError("You are not in a guild!");
         accept = false;
     }
     else if (bot->GetGuildId())
     {
+        LOG_ERROR("playerbots", "Bot '{}' is already in a guild. Declining invitation.", bot->GetName().c_str());
         botAI->TellError("Sorry, I am in a guild already");
         accept = false;
     }
-    else if (GET_PLAYERBOT_AI(inviter) && !sPlayerbotAIConfig->randomBotGuildNearby)
+    else if (!botAI->GetSecurity()->CheckLevelFor(PLAYERBOT_SECURITY_INVITE, false, inviter, true))
     {
-        botAI->TellError("Sorry, I don't want to join your guild :(");
-        accept = false;
-    }
-    else if (!GET_PLAYERBOT_AI(inviter) && !botAI->GetSecurity()->CheckLevelFor(PLAYERBOT_SECURITY_INVITE, false, inviter, true))
-    {
+        LOG_INFO("playerbots", "Bot '{}' does not have the required security level to join inviter '{}' guild.", bot->GetName().c_str(), inviter->GetName().c_str());
         botAI->TellError("Sorry, I don't want to join your guild :(");
         accept = false;
     }
 
+    // Decide to accept or decline based on checks and log the decision
     if (accept)
     {
+        LOG_INFO("playerbots", "Bot '{}' accepting guild invite from '{}', Guild ID: {}", bot->GetName().c_str(), inviter->GetName().c_str(), guildId);
         WorldPackets::Guild::AcceptGuildInvite data = WorldPacket(CMSG_GUILD_ACCEPT);
         bot->GetSession()->HandleGuildAcceptOpcode(data);
     }
     else
     {
+        LOG_INFO("playerbots", "Bot '{}' declining guild invite from '{}'.", bot->GetName().c_str(), inviter->GetName().c_str());
         WorldPackets::Guild::GuildDeclineInvitation data = WorldPacket(CMSG_GUILD_DECLINE);
         bot->GetSession()->HandleGuildDeclineOpcode(data);
     }
