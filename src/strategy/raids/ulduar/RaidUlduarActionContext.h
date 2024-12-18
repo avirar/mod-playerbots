c68e15fb5a068fe705fb19fdcbb87c8daa3c3982
avirar <luke.thorpe@gmail.com>
2024-12-19 00:17:47 +1100

    Update RaidUlduarActionContext.h

--- src/strategy/raids/ulduar/RaidUlduarActionContext.h
+++ src/strategy/raids/ulduar/RaidUlduarActionContext.h
@@ -19,6 +19,8 @@ public:
         creators["flame leviathan enter vehicle"] = &RaidUlduarActionContext::flame_leviathan_enter_vehicle;
         creators["ignis move construct to scorched ground"] = &RaidUlduarActionContext::ignis_move_construct_to_scorched_ground;
         creators["ignis move molten construct to water"] = &RaidUlduarActionContext::ignis_move_molten_construct_to_water;
+        creators["ignis choose target"] = &RaidUlduarActionContext::ignis_choose_target;
+
     }
 
 private:
@@ -26,7 +28,7 @@ private:
     static Action* flame_leviathan_enter_vehicle(PlayerbotAI* ai) { return new FlameLeviathanEnterVehicleAction(ai); }
     static Action* ignis_move_construct_to_scorched_ground(PlayerbotAI* ai) { return new IgnisMoveConstructToScorchedGroundAction(ai); }
     static Action* ignis_move_molten_construct_to_water(PlayerbotAI* ai) { return new IgnisMoveMoltenConstructToWaterAction(ai); }
-
+    static Action* ignis_choose_target(PlayerbotAI* ai) { return new IgnisChooseTargetAction(ai); }
 
 };
 
