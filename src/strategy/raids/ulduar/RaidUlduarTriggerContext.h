aec3283ab35cb56160b98ea7083bb765d643e880
avirar <luke.thorpe@gmail.com>
2024-12-18 22:58:37 +1100

    Update RaidUlduarTriggerContext.h

--- src/strategy/raids/ulduar/RaidUlduarTriggerContext.h
+++ src/strategy/raids/ulduar/RaidUlduarTriggerContext.h
@@ -17,11 +17,13 @@ public:
     {
         creators["flame leviathan on vehicle"] = &RaidUlduarTriggerContext::flame_leviathan_on_vehicle;
         creators["flame leviathan vehicle near"] = &RaidUlduarTriggerContext::flame_leviathan_vehicle_near;
+        creators["ignis move construct to scorched ground"] = &RaidUlduarTriggerContext::ignis_move_construct_to_scorched_ground;
     }
 
 private:
     static Trigger* flame_leviathan_on_vehicle(PlayerbotAI* ai) { return new FlameLeviathanOnVehicleTrigger(ai); }
     static Trigger* flame_leviathan_vehicle_near(PlayerbotAI* ai) { return new FlameLeviathanVehicleNearTrigger(ai); }
+    static Trigger* ignis_move_construct_to_scorched_ground(PlayerbotAI* ai) { return new IgnisMoveConstructToScorchedGroundTrigger(ai); }
 };
 
 #endif
