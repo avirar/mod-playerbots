b1409a6628d2b912f8f20c78db523e35e3369dde
avirar <luke.thorpe@gmail.com>
2024-12-27 14:08:32 +1100

    Update BlessingManager.h

--- src/strategy/paladin/BlessingManager.h
+++ src/strategy/paladin/BlessingManager.h
@@ -19,11 +19,17 @@ public:
     // Get assigned blessings for a specific Paladin
     std::vector<GreaterBlessingType> GetAssignedBlessings(PlayerbotAI* botAI) const;
     
+    // Get target classes for a specific blessing
+    std::vector<ClassID> GetTargetClasses(GreaterBlessingType blessingType) const;
+    
 private:
     PlayerbotAI* botAI;
     
     // Mapping from Paladin GUID to their assigned blessings
     std::map<ObjectGuid, std::vector<GreaterBlessingType>> paladinBlessings;
+    
+    // Mapping from class to which Paladin is blessing it
+    std::map<ClassID, ObjectGuid> classBlessingPaladinMap;
     
     // Helper function to get all Paladins in the raid
     std::vector<Player*> GetPaladinsInRaid() const;
