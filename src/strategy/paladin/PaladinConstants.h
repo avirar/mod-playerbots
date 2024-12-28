43b0da49039a9e3bfac9c4881612ce8a957c49d0
avirar <luke.thorpe@gmail.com>
2024-12-28 20:23:11 +1100

    Fixed ClassID Enums

--- src/strategy/paladin/PaladinConstants.h
+++ src/strategy/paladin/PaladinConstants.h
@@ -8,17 +8,17 @@
 // Enum for Class IDs
 enum ClassID
 {
-    WARRIOR = 1,
-    ROGUE,
-    PRIEST,
-    DRUID,
-    PALADIN,
-    HUNTER,
-    MAGE,
-    WARLOCK,
-    SHAMAN,
-    DEATHKNIGHT,
-    PET
+    CLASS_WARRIOR       = 1, // TITLE Warrior
+    CLASS_PALADIN       = 2, // TITLE Paladin
+    CLASS_HUNTER        = 3, // TITLE Hunter
+    CLASS_ROGUE         = 4, // TITLE Rogue
+    CLASS_PRIEST        = 5, // TITLE Priest
+    CLASS_DEATH_KNIGHT  = 6, // TITLE Death Knight
+    CLASS_SHAMAN        = 7, // TITLE Shaman
+    CLASS_MAGE          = 8, // TITLE Mage
+    CLASS_WARLOCK       = 9, // TITLE Warlock
+    //CLASS_UNK           = 10,
+    CLASS_DRUID         = 11 // TITLE Druid
 };
 
 // Enum for Greater Blessing Types
