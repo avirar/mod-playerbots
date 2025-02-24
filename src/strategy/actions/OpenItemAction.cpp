0605177e88562fb7233e8b0f5f9dc52b9a76c52c
avirar <luke.thorpe@gmail.com>
2025-02-25 09:25:46 +1100

    Separated OpenItem and UnlockItem

--- src/strategy/actions/OpenItemAction.cpp
+++ src/strategy/actions/OpenItemAction.cpp
@@ -1,15 +1,14 @@
 #include "OpenItemAction.h"
+#include "UnlockItemAction.h"
 #include "PlayerbotAI.h"
 #include "ItemTemplate.h"
 #include "WorldPacket.h"
 #include "Player.h"
 #include "ObjectMgr.h"
-#include "UseItemAction.h"
 
 bool OpenItemAction::Execute(Event event)
 {
     bool foundOpenable = false;
-    botAI->TellMaster("Checking for openable items...");
 
     // Check items in the bags
     for (uint8 bag = INVENTORY_SLOT_BAG_START; bag < INVENTORY_SLOT_BAG_END; ++bag)
@@ -22,24 +21,16 @@ bool OpenItemAction::Execute(Event event)
         {
             Item* item = bot->GetItemByPos(bag, slot);
 
-            if (item)
-            {
-                botAI->TellMaster("Found item: " + item->GetTemplate()->Name1);
-            }
-
             if (item && CanOpenItem(item))
             {
-                botAI->TellMaster("Item can be opened: " + item->GetTemplate()->Name1);
+                // Create an instance of UnlockItemAction
+                UnlockItemAction unlockAction(botAI);
 
-                if (UnlockItem(item, bag, slot))
+                // Attempt to unlock the item first
+                if (unlockAction.Execute(item, bag, slot))
                 {
-                    botAI->TellMaster("Unlocking item: " + item->GetTemplate()->Name1);
                     OpenItem(item, bag, slot);
                     foundOpenable = true;
-                }
-                else
-                {
-                    botAI->TellMaster("Failed to unlock: " + item->GetTemplate()->Name1);
                 }
             }
         }
@@ -48,7 +39,7 @@ bool OpenItemAction::Execute(Event event)
     // If no openable items were found
     if (!foundOpenable)
     {
-        botAI->TellMaster("No openable items in inventory.");
+        botAI->TellError("No openable items in inventory.");
     }
 
     return foundOpenable;
@@ -64,50 +55,7 @@ bool OpenItemAction::CanOpenItem(Item* item)
         return false;
 
     // Check if the item is openable
-    if (!(itemTemplate->Flags & ITEM_FLAG_HAS_LOOT))
-        return false;
-
-    // If the item has no lock, it can be opened freely
-    if (itemTemplate->LockID == 0)
-        return true;
-
-    // Get the lock entry for the item
-    LockEntry const* lockInfo = sLockStore.LookupEntry(itemTemplate->LockID);
-    if (!lockInfo)
-        return false;
-
-    // Declare variables outside of the switch block
-    SkillType requiredSkill = SKILL_NONE;
-    uint32 requiredSkillValue = 0;
-
-    // Check the lock requirements
-    for (uint8 i = 0; i < 8; ++i)
-    {
-        switch (lockInfo->Type[i])
-        {
-            case LOCK_KEY_ITEM:
-                // Check if the bot has the required key item
-                if (lockInfo->Index[i] > 0 && bot->HasItemCount(lockInfo->Index[i], 1))
-                    return true;
-                break;
-
-            case LOCK_KEY_SKILL:
-                // Assign values outside the switch block
-                requiredSkill = SkillByLockType(LockType(lockInfo->Index[i]));
-                requiredSkillValue = std::max((uint32)1, lockInfo->Skill[i]);
-
-                if (requiredSkill > 0 && bot->HasSkill(requiredSkill) && bot->GetSkillValue(requiredSkill) >= requiredSkillValue)
-                    return true;
-                break;
-
-            case LOCK_KEY_NONE:
-                // The item is not locked, it can be opened
-                return true;
-        }
-    }
-
-    // If none of the conditions were met, the bot cannot open the item
-    return false;
+    return (itemTemplate->Flags & ITEM_FLAG_HAS_LOOT);
 }
 
 void OpenItemAction::OpenItem(Item* item, uint8 bag, uint8 slot)
@@ -119,69 +67,4 @@ void OpenItemAction::OpenItem(Item* item, uint8 bag, uint8 slot)
     std::ostringstream out;
     out << "Opened item: " << item->GetTemplate()->Name1;
     botAI->TellMaster(out.str());
-}
-
-bool OpenItemAction::UnlockItem(Item* item, uint8 bag, uint8 slot)
-{
-    if (!item)
-        return false;
-
-    ItemTemplate const* itemTemplate = item->GetTemplate();
-    if (!itemTemplate || itemTemplate->LockID == 0)
-        return true; // No lock means it is already openable.
-
-    LockEntry const* lockInfo = sLockStore.LookupEntry(itemTemplate->LockID);
-    if (!lockInfo)
-        return false;
-
-    SkillType requiredSkill = SKILL_NONE;
-    uint32 requiredSkillValue = 0;
-    uint32 requiredKeyItem = 0;
-
-    // Scan for lock requirements
-    for (uint8 i = 0; i < 8; ++i)
-    {
-        switch (lockInfo->Type[i])
-        {
-            case LOCK_KEY_SKILL:
-                // Prioritize skill-based unlocking
-                requiredSkill = SkillByLockType(LockType(lockInfo->Index[i]));
-                requiredSkillValue = std::max((uint32)1, lockInfo->Skill[i]);
-
-                if (requiredSkill > 0 && bot->HasSkill(requiredSkill) && bot->GetSkillValue(requiredSkill) >= requiredSkillValue)
-                {
-                    bot->CastSpell(bot, lockInfo->Index[i], TRIGGERED_NONE);
-                    botAI->TellMaster("Using skill to unlock item: " + item->GetTemplate()->Name1);
-                    return true;
-                }
-                break;
-
-            case LOCK_KEY_ITEM:
-                // Store the required key item for later use
-                if (lockInfo->Index[i] > 0)
-                    requiredKeyItem = lockInfo->Index[i];
-                break;
-
-            case LOCK_KEY_NONE:
-                return true; // No unlocking required.
-        }
-    }
-
-    // If skill unlocking was not possible, attempt to use a key item
-    if (requiredKeyItem > 0 && bot->HasItemCount(requiredKeyItem, 1))
-    {
-        Item* keyItem = bot->GetItemByEntry(requiredKeyItem);
-        if (keyItem)
-        {
-            // Use the key item correctly
-            if (UseItem(keyItem, ObjectGuid::Empty, item))
-            {
-                botAI->TellMaster("Used key to unlock item: " + item->GetTemplate()->Name1);
-                return true;
-            }
-        }
-    }
-
-    botAI->TellMaster("Failed to unlock item: " + item->GetTemplate()->Name1);
-    return false;
 }
