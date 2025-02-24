8921188ed9c2e57fab5f4b1f03f62f8c45b470b0
avirar <luke.thorpe@gmail.com>
2025-02-25 09:22:54 +1100

    Create UnlockItemAction.h

---
+++ src/strategy/actions/UnlockItemAction.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license,
+ * you may redistribute it and/or modify it under version 2 of the License, or (at your option), any later version.
+ */
+
+#ifndef _PLAYERBOT_UNLOCKITEMACTION_H
+#define _PLAYERBOT_UNLOCKITEMACTION_H
+
+#include "UseItemAction.h"
+
+class Player;
+class Item;
+class Event;
+
+class UnlockItemAction : public UseItemAction
+{
+public:
+    UnlockItemAction(PlayerbotAI* botAI) : UseItemAction(botAI, "unlock item") { }
+
+    // Attempts to unlock an item before it can be opened
+    bool Execute(Item* item, uint8 bag, uint8 slot);
+};
+
+#endif
