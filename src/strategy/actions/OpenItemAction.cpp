f9108c9b77ddb4aeb1fbc52b2c637733de29f030
avirar <luke.thorpe@gmail.com>
2025-02-25 18:40:28 +1100

    Update OpenItemAction.cpp

--- src/strategy/actions/OpenItemAction.cpp
+++ src/strategy/actions/OpenItemAction.cpp
@@ -27,7 +27,7 @@ bool OpenItemAction::Execute(Event event)
                 UnlockItemAction unlockAction(botAI);
 
                 // Attempt to unlock the item first
-                if (unlockAction.Execute(Event event))
+                if (unlockAction.Execute(event))
                 {
                     OpenItem(item, bag, slot);
                     foundOpenable = true;
