245a0ee4ca13424e553bbd0eefd206eef66fc20f
avirar <luke.thorpe@gmail.com>
2025-08-22 14:46:29 +1000

    Create UseQuestItemsStrategy.cpp

---
+++ src/strategy/generic/UseQuestItemsStrategy.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license, you may redistribute it
+ * and/or modify it under version 2 of the License, or (at your option), any later version.
+ */
+
+#include "UseQuestItemsStrategy.h"
+
+#include "Playerbots.h"
+
+void UseQuestItemsStrategy::InitTriggers(std::vector<TriggerNode*>& triggers)
+{
+    /*
+    triggers.push_back(new TriggerNode("quest item usable", NextAction::array(0, new NextAction("quest item use on target", 6.0f), nullptr)));
+    triggers.push_back(
+        new TriggerNode("far from quest item target", NextAction::array(0, new NextAction("move to quest item target", 7.0f), nullptr)));
+    triggers.push_back(new TriggerNode("often", NextAction::array(0, new NextAction("quest item usable", 5.0f), nullptr)));
+    */
+}
