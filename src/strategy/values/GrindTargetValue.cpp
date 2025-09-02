66793b8468507d410182bf0052461b173252406a
avirar <luke.thorpe@gmail.com>
2025-09-02 11:43:51 +1000

    Update GrindTargetValue.cpp

--- src/strategy/values/GrindTargetValue.cpp
+++ src/strategy/values/GrindTargetValue.cpp
@@ -82,7 +82,7 @@ Unit* GrindTargetValue::FindTargetForGrinding(uint32 assistCount)
 
         // Allow targeting flying creatures - all classes have some ranged abilities, 25yds, -2 for buffer)
         // Use spell distance since even melee classes have ranged attacks (Heroic Throw, ranged weapons, etc.)
-        if (abs(bot->GetPositionZ() - unit->GetPositionZ()) > 23.0f)
+        if ((abs(bot->GetPositionZ() - unit->GetPositionZ()) > 23.0f) && !needForQuest(unit))
             continue;
 
         if (!bot->InBattleground() && GetTargetingPlayerCount(unit) > assistCount)
